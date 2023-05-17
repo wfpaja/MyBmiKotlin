f845c973ab92e310cda5ebb24c7f37b72d6d8539
goodsuper <givemejapan@gmail.com>
2023-05-18 03:58:14 +0800

    還在想adapter要怎麼寫

--- app/src/main/java/com/example/mybmikotlin/CategoryAdapter.kt
+++ app/src/main/java/com/example/mybmikotlin/CategoryAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.mybmikotlin
 
+import android.content.Context
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
@@ -7,8 +8,10 @@ import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.example.mybmikotlin.data.Info
 import com.example.mybmikotlin.databinding.CategoryItemViewBinding
 
-class CategoryAdapter(viewModel: BmiViewModel):
+class CategoryAdapter(val context: Context, private val viewModel: BmiViewModel):
     ListAdapter<List<Info>, CategoryAdapter.ItemViewHolder>(DiffCallback) {
+//    private val categoryList:List<CategoryData> by lazy { categoryListInit() }
+
     class ItemViewHolder(private var binding: CategoryItemViewBinding):
         ViewHolder(binding.root) {
         fun bind(dataList: List<Info>) {
@@ -27,6 +30,11 @@ class CategoryAdapter(viewModel: BmiViewModel):
         TODO("Not yet implemented")
     }
 
+//    private fun categoryListInit(): List<CategoryData> {
+//        val categories = arrayOf(Category.THIN, Category.NORMAL, Category.FAT)
+//        val titleTexts = arrayOf(context.getString(R.string.thin_format))
+//    }
+
     companion object {
         private val DiffCallback = object : DiffUtil.ItemCallback<List<Info>>() {
             override fun areItemsTheSame(oldItem: List<Info>, newItem: List<Info>): Boolean {
@@ -40,3 +48,9 @@ class CategoryAdapter(viewModel: BmiViewModel):
         }
     }
 }
+
+enum class Category {
+    THIN, NORMAL, FAT
+}
+
+data class CategoryData(val titleText: String, val category: Category)
